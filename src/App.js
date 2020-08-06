86a4066b5ac178f20c363f8d519aabcedb9917cc
Ekata Tandukar <tandukarekata13@gmail.com>
2020-08-06 13:42:01 +0545

    post is undefined

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 
+import store from './store';
+
 class App extends Component {
   render() {
     return (
